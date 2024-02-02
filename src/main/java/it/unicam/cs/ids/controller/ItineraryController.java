0c7ef7dff03296916166d04a512e5b8b3b8b38ef
Alessio <alessio.marinelli@studenti.unicam.it>
2024-02-02 11:33:26 +0100

    Itinerary Controller Test class

--- src/main/java/it/unicam/cs/ids/controller/ItineraryController.java
+++ src/main/java/it/unicam/cs/ids/controller/ItineraryController.java
@@ -1,23 +1,32 @@
 package it.unicam.cs.ids.controller;
 
 import it.unicam.cs.ids.model.Itinerary;
+import it.unicam.cs.ids.model.user.BaseUser;
 import it.unicam.cs.ids.model.user.IUserPlatform;
 import it.unicam.cs.ids.model.user.UserRole;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItineraryController {
 
     List<Itinerary> itineraries;
-
     List<Itinerary> pendingItinerary;
 
-    public void addItinerary(Itinerary itinerary , IUserPlatform user){
-        if(user.getUserType().equals(UserRole.Contributor))
-            this.addWithPending(itinerary);
-        else
-            this.addWithoutPending(itinerary);
+
+    public ItineraryController() {
+        this.itineraries = new ArrayList<Itinerary>();
+        this.pendingItinerary = new ArrayList<Itinerary>();
     }
 
+    public void addItinerary(Itinerary itinerary , IUserPlatform user) {
+        if (!(user.getUserType().equals(UserRole.Tourist) || user.getUserType().equals(UserRole.Animator))) {
+            if (user.getUserType().equals(UserRole.Contributor))
+                this.addWithPending(itinerary);
+            else
+                this.addWithoutPending(itinerary);
+        }
+    }
     private void addWithPending(Itinerary itinerary){
         itinerary.setValidation(false);
         this.pendingItinerary.add(itinerary);
@@ -28,13 +37,24 @@ public class ItineraryController {
         this.itineraries.add(itinerary);
     }
 
-    public void validateItinerary(boolean choice, Itinerary itinerary){
-        int index = this.itineraries.indexOf(itinerary);
-        Itinerary itineraryToValidate = this.itineraries.get(index);
-        if(choice)
-            itineraryToValidate.setValidation(true);
-        else
-            this.itineraries.remove(itineraryToValidate);
+    public void validateItinerary(BaseUser curator, boolean choice, Itinerary itinerary) {
+        if (curator.getUserType().equals(UserRole.Curator)) {
+            int index = this.pendingItinerary.indexOf(itinerary);
+            if (index != -1) {
+                if (choice) {
+                    //   pendingItinerary.setValidation(true);
+                    itineraries.add(itinerary);
+                    this.pendingItinerary.remove(itinerary);
+                } else
+                    this.pendingItinerary.remove(itinerary);
+            }
+        }
     }
 
+    public int getItinerariesSize(){
+        return this.itineraries.size();
+    }
+    public int getPendingItinerariesSize(){
+        return this.pendingItinerary.size();
+    }
 }
