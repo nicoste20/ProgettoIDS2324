792df3026ab0b6910363ee5699a0ae64ad37fed4
P-Michele <perinimichele0@gmail.com>
2024-02-08 10:17:29 +0100

    Adding Springboot to the project

---
+++ src/main/java/it/unicam/cs/ids/IdsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdsSpringbootApplication.class, args);
+	}
+
+}
