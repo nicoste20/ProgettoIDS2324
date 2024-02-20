30cef724ed2122b93a17ee4ad24ac79790286c9e
P-Michele <perinimichele0@gmail.com>
2024-02-20 19:37:34 +0100

    Implementation and editing of comments, allowing their use via DB

--- src/main/java/it/unicam/cs/ids/controller/Repository/CommentRepository.java
+++ src/main/java/it/unicam/cs/ids/controller/Repository/CommentRepository.java
@@ -4,5 +4,8 @@ import it.unicam.cs.ids.model.content.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment,Integer> {
+    List<Comment> findByContentId(Integer contentId);
 }
--- src/main/java/it/unicam/cs/ids/model/content/Comment.java
+++ src/main/java/it/unicam/cs/ids/model/content/Comment.java
@@ -1,38 +1,81 @@
 package it.unicam.cs.ids.model.content;
 
 import it.unicam.cs.ids.model.user.BaseUser;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 
 /**
  *  Represents a base implementation of the {@link Content} interface.
  * This class provides methods that concerns about Comments.
  */
 @Entity
-public class Comment extends Content {
-    @OneToOne(targetEntity = Content.class)
-    private Content content;
+public class Comment{
+
+    private boolean isValidate;
+
+    private int contentId;
+
+    private int authorId;
+
+    private String comment;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
 
     /**
      * Constructs a new Contest object with the specified parameters.
      *
-     * @param name  The text of a comment
-     * @param content The content associated with a comment.
+     * @param comment  The text of a comment
+     * @param contentId The content associated with a comment.
      */
-    public Comment(String name,Content content, String description) {
-        super(name,description);
-        this.content = content;
+    public Comment(int contentId, String comment) {
+        this.contentId = contentId;
+        this.isValidate = false;
+        this.comment = comment;
     }
 
     public Comment() {
-        super(null, null);
+        this.isValidate = false;
     }
 
-    /**
-     *it gets the content of the comment
-     */
-    public Content getContent() {
-        return this.content;
+    public int getContentId() {
+        return contentId;
     }
 
+    public void setContentId(int contentId) {
+        this.contentId = contentId;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isValidate() {
+        return isValidate;
+    }
+
+    public void setValidation(boolean validate) {
+        isValidate = validate;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
