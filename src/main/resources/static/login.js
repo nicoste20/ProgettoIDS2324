cd75ae470c490cc58c09d1a6cf4085d6400f5f05
Alessio <alessio.marinelli@studenti.unicam.it>
2024-02-22 09:15:42 +0100

    added the users and made some changes

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -5,19 +5,19 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     var email = document.getElementById('email').value.toLowerCase();
     switch (true){
         case email.includes('animatore'):
-            window.location.href = 'animatore.html';
+            window.location.href = '/templates/UsersType/animatore.html';
             break;
         case email.includes('contributorautorizzato'):
-            window.location.href = 'contributoAutorizzato.html';
+            window.location.href = '/templates/UsersType/contributorAutorizzato.html';
             break;
         case email.includes('curatore'):
-            window.location.href = 'curatore.html';
+            window.location.href = '/templates/UsersType/curatore.html';
             break;
         case email.includes('turistaautorizzato'):
-            window.location.href = 'turistaAutorizzato.html';
+            window.location.href = '/templates/UsersType/turistaAutorizzato.html';
             break;
         case email.includes('gestorepiattaforma'):
-            window.location.href = 'gestorePiattaforma.html';
+            window.location.href = '/templates/UsersType/gestorePiattaforma.html';
             break;
         default:
             alert('Effettua il login');
