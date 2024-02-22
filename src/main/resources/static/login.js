dbb52a3b4e7f8365cabe62c0cc8be6a91cfb9ccd
Alessio <alessio.marinelli@studenti.unicam.it>
2024-02-22 11:34:12 +0100

    add comments section in "post" and the relative css. Added directories for each user

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -8,18 +8,19 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
             window.location.href = '/templates/UsersType/animatore.html';
             break;
         case email.includes('contributorautorizzato'):
-            window.location.href = '/templates/UsersType/contributorAutorizzato.html';
+            window.location.href = '../templates/UsersType/contributorAutorizzato/contributorAutorizzato.html';
             break;
         case email.includes('curatore'):
-            window.location.href = '/templates/UsersType/curatore.html';
+            window.location.href = '/templates/UsersType/curatore/curatore.html';
             break;
         case email.includes('turistaautorizzato'):
-            window.location.href = '/templates/UsersType/turistaAutorizzato.html';
+            window.location.href = '/templates/UsersType/turistaAutorizzato/turistaAutorizzato.html';
             break;
         case email.includes('gestorepiattaforma'):
-            window.location.href = '/templates/UsersType/gestorePiattaforma.html';
+            window.location.href = '/templates/UsersType/gestorePiattaforma/gestorePiattaforma.html';
             break;
         default:
             alert('Effettua il login');
     }
 });
+
